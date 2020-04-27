4074e5a69a74af5683a3e6ed0e94a6fa002eddab
1m0d <domi.debreczeni@gmail.com>
2020-04-28 01:08:44 +0200

    create singleton GameController class, instead of new

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,17 +8,18 @@ import java.util.Scanner;
 public class Main {
 
     public Map map;
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
     {
-        GameController gameController = new GameController();
+        GameController gameController = GameController.getInstance();
         Interpreter interpreter = new Interpreter();
         String[] testCasePaths = {
                 "00-load-map ","01-player-steps-onto-regular-tile","02-player-steps-onto-hole-tile","03-player-uses-eskimo-ability","04-player-uses-scientist-ability",
                 "05-player-passes","06-player-picks-up-an-item","07-player-clears-snow","08-player-uses-diving-suit","09-player-uses-food","10-player-uses-winning-item","11-player-uses-rope",
                 "12-player-uses-shovel","13-player-uses-fragile-shovel","14-player-uses-fragile-shovel-and-it-breaks","15-player-uses-camp","16-gamecontroller-generates-storm","17-bear-steps",
-                "18-bear-meets-player"};
-        for (int i = 0; i < testCasePaths.length; i++)
-        {
+                "18-bear-meets-player"
+        };
+
+        for (int i = 0; i < testCasePaths.length; i++) {
             System.out.println(i + ":\t" + testCasePaths[i]);
         }
 
