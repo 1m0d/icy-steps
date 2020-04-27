2ca66153a278cd2d18770953e8a81b7d3aa641db
1m0d <domi.debreczeni@gmail.com>
2020-04-28 01:19:17 +0200

    fix player classes

--- src/modules/Scientist.java
+++ src/modules/Scientist.java
@@ -1,27 +1,15 @@
 package modules;
 
 public class Scientist extends Player {
-    public Scientist(int Id)
-    {
-        super();
+
+    public Scientist(Tile position) {
+        super(position);
     }
 
-    public Scientist(Tile t, String token1, String token2, String token3, String token4) {
-        super(t, token1, token2, token3, token4);
-        maxLives = 4;
-    }
-
-    @Override
-    public void turn()
-    {
-        System.out.println( this.toString() + "turn was called");
+    public Scientist(Tile position, int energy, int lives, boolean drowning, int uniqueID) {
+        super(position, energy, lives, drowning, uniqueID);
     }
 
     @Override
-    public void useAbility(Tile t) {
-        System.out.println( this.toString() + " useAbility was called with param: " + t.toString());
-        t.onScientistAbility();
-    }
-
-
+    public void useAbility(Tile t) { t.onScientistAbility(); }
 }
