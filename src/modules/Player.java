f8468c58821746aba8fd8344b7ca68acddff8d85
nellics97 <nellics97@gmail.com>
2020-04-27 23:53:52 +0200

    modifying player class

--- src/modules/Player.java
+++ src/modules/Player.java
@@ -1,5 +1,7 @@
 package modules;
 
+import java.util.List;
+
 public abstract class Player
 {
     public static int maxLives;
@@ -8,6 +10,8 @@ public abstract class Player
     protected int lives;
     protected boolean drowning = false;
     protected int uniqueID;
+    protected List<Item> items;
+    int nOfWinningItems = 0;
 
     protected boolean hasDivingSuit = false;
 
@@ -28,31 +32,36 @@ public abstract class Player
 
     public Player getPlayer() {
         System.out.println( this.toString() + " getPlayer was called");
-        onFood();
         return this;
     }
 
     public void addItemToInventory(Item i)
     {
         System.out.println( this.toString() + " addItemToInventory was called with param: " + i.toString());
+        items.add(i);
+
     }
 
     boolean work()
     {
         System.out.println( this.toString() + " work was called");
-        if(energy > 0)
+        if(energy != 0)
         {
             energy--;
             return true;
         }
 
-        System.out.println("Nincs több energiád!");
+        System.out.println("You don't have more energy");
         return false;
     }
 
     public void turn()
     {
         System.out.println( this.toString() + " turn was called");
+        while(energy != 0){
+            work();
+        }
+
     }
 
     public void step(Tile t) {
@@ -63,29 +72,38 @@ public abstract class Player
     public void pass()
     {
         System.out.println( this.toString() + " pass was called");
+        energy=0;
     }
 
     public void useItem(Item i) {
         System.out.println( this.toString() + " useItem was called with param: " + i.toString());
+        i.useItem(position);
     }
 
     public void useAbility(Tile t) {
         System.out.println( this.toString() + " useAbility was called with param: " + t.toString());
+
     }
 
     public void onStorm()
     {
         System.out.println( this.toString() + " onStorm was called");
+        position.onStorm();
     }
 
     public void onFood()
     {
         System.out.println( this.toString() + " onFood was called");
+        if (lives < maxLives) {
+            lives++;
+        }
+
     }
 
     public void onHole()
     {
         System.out.println( this.toString() + " onHole was called");
+        drowning = true;
     }
 
     public void getPulledTo(Tile t) {
@@ -96,12 +114,22 @@ public abstract class Player
     public void clearSnow()
     {
         System.out.println( this.toString() + " clearSnow was called");
+        position.onShovel();
+
     }
 
     public void setDivingSuit()
     {
         System.out.println( this.toString() + " setDivingSuit was called");
+        hasDivingSuit = true;
     }
 
-    public void damage(){}
-}
+    public void damage() {
+        lives--;
+        }
+
+     public int getWinningItemN() {
+         return nOfWinningItems;}
+
+    }
+
