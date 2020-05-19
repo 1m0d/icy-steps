6be2349f81d519d709246e3de52d0ee617923bbd
nellics97 <nellics97@gmail.com>
2020-05-20 00:00:55 +0200

    inventory

--- src/gui/GameView.java
+++ src/gui/GameView.java
@@ -1,10 +1,15 @@
 package gui;
 import modules.GameModel;
 import modules.Player;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class GameView extends JPanel {
@@ -20,6 +25,12 @@ public class GameView extends JPanel {
         model.getMap().Draw(this);
     }
 
+    private static Icon resizeIcon(ImageIcon icon) {
+        Image img = icon.getImage();
+        Image resizedImage = img.getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(resizedImage);
+    }
+
     private void initializeToolbar(){
         JFrame frame = new JFrame("Navigate");
         JButton stepButton, passButton, useItemButton, clearSnowButton,pickUpItemButton, useAbilityButton;
@@ -27,18 +38,36 @@ public class GameView extends JPanel {
         stepButton = new JButton("Step");
         stepDirCB = new JComboBox();
         stepDirCB.addItem("up");
-        stepDirCB.addItem("up and right"); //HEXAGON
-        stepDirCB.addItem("down and right");
+        stepDirCB.addItem("right");
         stepDirCB.addItem("down");
-        stepDirCB.addItem("down and left");
-        stepDirCB.addItem("up and left");
+        stepDirCB.addItem("left");
         passButton = new JButton("Pass");
         useItemButton = new JButton("Use Item");
         clearSnowButton = new JButton("Clear Snow");
         pickUpItemButton = new JButton("Pick Up Item");
         useAbilityButton = new JButton("Use Ability");
-        Integer[] ids = {0, 1, 2, 3, 4};
+        Integer[] ids = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}; //ezt nem igy kellene probably
         useAbilityCB = new JComboBox(ids);
+
+        ImageIcon cImage =  new ImageIcon("src/gui/icons/camp.png");
+        JButton cButton = new JButton(resizeIcon(cImage));
+        ImageIcon dsImage =  new ImageIcon("src/gui/icons/divingsuit.gif");
+        JButton dsButton = new JButton(resizeIcon(dsImage));
+        ImageIcon fImage =  new ImageIcon("src/gui/icons/food.jpg");
+        JButton foodButton = new JButton(resizeIcon(fImage));
+        ImageIcon fsImage =  new ImageIcon("src/gui/icons/fragileshovel.png");
+        JButton fstButton = new JButton(resizeIcon(fsImage));
+        ImageIcon rImage =  new ImageIcon("src/gui/icons/rope.png");
+        JButton rButton = new JButton(resizeIcon(rImage));
+        ImageIcon sImage =  new ImageIcon("src/gui/icons/shovel.gif");
+        JButton sButton = new JButton(resizeIcon(sImage));
+        ImageIcon w1Image =  new ImageIcon("src/gui/icons/winningitem1.png");
+        JButton w1Button = new JButton(resizeIcon(w1Image));
+        ImageIcon w2Image =  new ImageIcon("src/gui/icons/winningitem2.png");
+        JButton w2Button = new JButton(resizeIcon(w2Image));
+        ImageIcon w3Image =  new ImageIcon("src/gui/icons/winningitem3.png");
+        JButton w3Button = new JButton(resizeIcon(w3Image));
+
 
         //Action listeners
         passButton.addActionListener(new ActionListener() {
@@ -93,31 +122,38 @@ public class GameView extends JPanel {
         frame.add(useAbilityCB);
         frame.add(clearSnowButton);
         frame.add(pickUpItemButton);
-        frame.add(useItemButton);
+
+        frame.add(cButton);
+        frame.add(dsButton);
+        frame.add(foodButton);
+        frame.add(fstButton);
+        frame.add(rButton);
+        frame.add(sButton);
+        frame.add(w1Button);
+        frame.add(w2Button);
+        frame.add(w3Button);
+
+
         frame.setLayout(new FlowLayout());
-        frame.setSize(300,300);
+        frame.setSize(250,500);
         frame.setVisible(true);
     }
 
 
     public GameView() {
         super();
-        JFrame frame = new JFrame();
-        JPanel pane1 = new JPanel();
-        JPanel pane2 = new JPanel();
-        frame.add(pane1, BorderLayout.WEST);
-        frame.add(pane2, BorderLayout.EAST);
         GridLayout gridLayout = new GridLayout(nOfRows, nOfColumns);
         gridLayout.setHgap(5);
         gridLayout.setVgap(5);
         setLayout(gridLayout);
+        initializeToolbar();
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         doDrawing(g);
-        initializeToolbar();
+
     }
 
 }
